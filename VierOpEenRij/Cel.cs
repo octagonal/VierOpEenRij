cdcda734ea2503476cdadbadfef949b000d6fd59
Anthony Madhvani <anny.mdvn@gmail.com>
2014-04-22 10:43:28 +0200

    Clean up Cel and HitBox interfaces

--- VierOpEenRij/Cel.cs
+++ VierOpEenRij/Cel.cs
@@ -11,41 +11,41 @@ using Windows.UI.Xaml.Shapes;
 
 namespace VierOpEenRij
 {
-    public class Cel : Grid
-    {
-        //Achtergrond
-        //Border thickness en kleur anders dan Achtergrond
-	private Ellipse ellipse = new Ellipse();
-	public int rowPos;
-	public int colPos;
-	public int speler = -1;
-	public Boolean vrij = true;
-	public static Color[] coloring = { Colors.Green, Colors.Yellow, Colors.Red, Colors.Blue, Colors.OrangeRed };
-
-        public Cel(double width, double height, int rowPos, int colPos)
-        {
-            //this.Background = new SolidColorBrush(Colors.Blue);
-            Border border = new Border() {
-                BorderBrush = new SolidColorBrush(Colors.Black),
-                BorderThickness = new Windows.UI.Xaml.Thickness(1, 1, 1, 1),
-		CornerRadius = new Windows.UI.Xaml.CornerRadius(5, 5, 5, 5)
-            };
-            this.Children.Add(border);
-	    ellipse.Fill = new SolidColorBrush(Colors.Transparent);
-	    this.Children.Add(ellipse);
-
-	    this.Width = width;
-	    this.Height = height;
-	    this.colPos = colPos;
-	    this.rowPos = rowPos;
-        }
-
-	public void maakBezet(int huidigeSpeler)
+	public class Cel : Grid
 	{
-		vrij = false;
-		speler = huidigeSpeler;
-		this.Background = new SolidColorBrush(coloring[speler]);
-	}
+		private Ellipse ellipse = new Ellipse();
+		public int rowPos;
+		public int colPos;
+		public int speler = -1;
+		public Boolean vrij = true;
+		public static Color[] coloring = { Colors.Green, Colors.Yellow, Colors.Red, Colors.Blue, Colors.OrangeRed };
 
-    }
+		public Cel(double width, double height, int rowPos, int colPos)
+		{
+			Border border = new Border()
+			{
+				BorderBrush = new SolidColorBrush(Colors.Black),
+				BorderThickness = new Windows.UI.Xaml.Thickness(0.1, 0.1, 0.1, 0.1),
+				CornerRadius = new Windows.UI.Xaml.CornerRadius(2, 2, 2, 2)
+			};
+			this.Children.Add(border);
+
+			this.Width = width;
+			this.Height = height;
+			this.colPos = colPos;
+			this.rowPos = rowPos;
+
+			this.SetValue(Canvas.TopProperty, height * rowPos);
+			this.SetValue(Canvas.LeftProperty, width * colPos);
+			this.IsHitTestVisible = false;
+		}
+
+		public void maakBezet(int huidigeSpeler)
+		{
+			vrij = false;
+			speler = huidigeSpeler;
+			this.Background = new SolidColorBrush(coloring[speler]);
+		}
+
+	}
 }
--- VierOpEenRij/Spelbord.cs
+++ VierOpEenRij/Spelbord.cs
@@ -32,20 +32,10 @@ namespace VierOpEenRij
 		}
 	}
 
-	/*
-	 * Lambda voor coord checken:
-	 * delegate int del(int i) ::
-	 * static void Main(string[] args)
-	 * {
-	 *	del myDelegate = x => x * x;
-	 *	int j = myDelegate(5); //j = 25
-	 * }
-	 */
-
 	public class Spelbord : Canvas
 	{
 		public const int KOLOMMEN = 7;
-		public const int RIJEN = 1;
+		public const int RIJEN = 5;
 		public const int AANTAL_SPELERS = 1;
 		public const int AANTAL_OP_RIJ = 4;
 
@@ -84,7 +74,6 @@ namespace VierOpEenRij
 			kolw = this.ActualWidth / KOLOMMEN;
 			rijh = this.ActualHeight / RIJEN;
 			VulGridOp();
-			BouwGrid();
 		}
 
 		public int SameSeek(Cel celin, Directions directions) {
@@ -104,21 +93,18 @@ namespace VierOpEenRij
 		{
 			foreach (Cel celCur in winContainer)
 			{
-				Debug.WriteLine("[" + celCur.colPos + "|" + celCur.rowPos + "]");
 				Border border = new Border()
 				{
 					BorderBrush = new SolidColorBrush(Colors.Orange),
 					BorderThickness = new Windows.UI.Xaml.Thickness(5, 5, 5, 5)
 				};
 				celCur.Children.Add(border);
-				//celCur.Background = new SolidColorBrush(Colors.Orange);
 			}
-			Debug.WriteLine("---");
 		}
 
 		private int TelPunten(Cel celin, Direction direction) {
 
-			int huidige_op_rij = 1; //De huidige cel telt ook al voor een punt
+			int huidige_op_rij = 1; 
 			Cel origCel = celin;
 			Cel currCel = celin;
 
@@ -158,7 +144,6 @@ namespace VierOpEenRij
 					break;
 				}
 			}
-			Debug.WriteLine("---");
 		}
 
 		private void CelCheck(Cel cel)
@@ -169,40 +154,14 @@ namespace VierOpEenRij
 			SameSeek(cel, verticaal);
 		}
 
-		public void BouwGrid()
-		{
-			/* 
-			 * Bouwt grid op
-			 * aan de hand van
-			 * de KOLOMMEN en RIJEN const
-			 */
-			for (int row = 0; row < RIJEN; row++){
-
-
-				for (int col = 0; col < KOLOMMEN; col++)
-				{
-					Cel cel = celContainer[row, col];
-					this.Children.Add(cel);
-				}
-			}
-		}
-
 		public void VulGridOp()
 		{
-			/*
-			 * Vul elke cel met een
-			 * Cel instantie
-			 */
-
 			for (int rij = 0; rij < RIJEN; rij++)
 			{
 				for (int kol = 0; kol < KOLOMMEN; kol++)
 				{
-					Cel cel = new Cel(kolw, rijh, rij, kol);	
-					cel.SetValue(Canvas.TopProperty, rijh * rij);
-					cel.SetValue(Canvas.LeftProperty, kolw * kol);
-					cel.IsHitTestVisible = false;
-					celContainer[rij, kol] = cel;
+					celContainer[rij, kol] = new Cel(kolw, rijh, rij, kol);	
+					this.Children.Add(celContainer[rij, kol]);
 				}
 			}
 
